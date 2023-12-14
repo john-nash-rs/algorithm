5243e2099ec4cd7b1b7019e14762cc56fda4d5b0
Harsh Vardhan <33624864+john-nash-rs@users.noreply.github.com>
2023-12-14 20:23:24 +0530

    Create Leetcode684.java

---
+++ leetcode/Leetcode684.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int[] parent = new int[n + 1];
+        
+        // Initialize parent array
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+        
+        int[] result = new int[2];
+        
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            int parentU = find(parent, u);
+            int parentV = find(parent, v);
+            
+            // Check if the vertices have the same parent
+            if (parentU == parentV) {
+                // This edge will form a cycle, return it
+                result[0] = u;
+                result[1] = v;
+            } else {
+                // Union the sets of u and v
+                parent[parentU] = parentV;
+            }
+        }
+        
+        return result;
+    }
+    
+    // Find operation with path compression
+    private int find(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = find(parent, parent[node]);
+        }
+        return parent[node];
+    }
+}
